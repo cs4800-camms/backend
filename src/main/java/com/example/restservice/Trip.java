61b598ab0e1feffd8389dd0efc9e40a2f263f946
stephaniedeleon <stephanie.deleon516@gmail.com>
2022-10-04 09:07:20 -0700

    added new method that uses lombok library

--- src/main/java/com/example/restservice/Trip.java
+++ src/main/java/com/example/restservice/Trip.java
@@ -1,25 +1,13 @@
 package com.example.restservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Trip {
+
     private final long id;
     private final String name;
     private final String destination;
-
-    public Trip(long id, String name, String destination) {
-        this.id = id;
-        this.name = name;
-        this.destination = destination;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
 }
