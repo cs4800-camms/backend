66d212b0014f8e2475005f62152aa8095fd817cf
stephaniedeleon <stephanie.deleon516@gmail.com>
2022-10-18 10:01:12 -0700

    added getTrips test in TripServiceTest

--- src/test/java/com/samm/trippytravel/service/TripServiceTest.java
+++ src/test/java/com/samm/trippytravel/service/TripServiceTest.java
@@ -1,7 +1,30 @@
 package com.samm.trippytravel.service;
 
+import com.samm.trippytravel.repository.TripRepository;
+import com.samm.trippytravel.services.TripService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class TripServiceTest {
+
+    @Mock
+    private TripRepository tripRepository;
+
+    private TripService tripService;
+
+    @BeforeEach
+    void setUp() {
+        this.tripService = new TripService(this.tripRepository);
+    }
+
+    @Test
+    void getTrips() {
+        tripService.getTrips();
+        verify(tripRepository).findAll();
+    }
 }
