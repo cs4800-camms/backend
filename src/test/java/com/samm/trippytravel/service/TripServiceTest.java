33ff6ef8ddd718473a8b9611d5949a0368ffdcd6
Nisha tourjob <nishatourjob@b01-aruba-authenticated-10-110-204-68.wlan.cpp.edu>
2022-10-18 11:45:54 -0700

    Added updateTrip test in TripServiceTest

--- src/test/java/com/samm/trippytravel/service/TripServiceTest.java
+++ src/test/java/com/samm/trippytravel/service/TripServiceTest.java
@@ -2,6 +2,7 @@ package com.samm.trippytravel.service;
 
 import com.samm.trippytravel.data.domain.Trip;
 import com.samm.trippytravel.data.requests.CreateTripRequest;
+import com.samm.trippytravel.data.requests.UpdateTripRequest;
 import com.samm.trippytravel.repository.TripRepository;
 import com.samm.trippytravel.services.TripService;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,12 +17,13 @@ public class TripServiceTest {
 
     @Mock
     private TripRepository tripRepository;
-
     private TripService tripService;
-
     private CreateTripRequest createTripRequest;
 
     private Trip trip;
+    private Trip updateTrip;
+    private UpdateTripRequest updateTripRequest;
+    private String id ="abcdefg";
 
     @BeforeEach
     void setUp() {
@@ -36,6 +38,17 @@ public class TripServiceTest {
                 .name("Mary")
                 .destination("Rome")
                 .build();
+        this.updateTrip = Trip.builder()
+                ._id(id)
+                .userId(4)
+                .name("Mel")
+                .destination("Japan")
+                .build();
+        this.updateTripRequest = UpdateTripRequest.builder()
+                .userId(4)
+                .name("Mel T.")
+                .destination("Taipe")
+                .build();
     }
 
     @Test
@@ -49,4 +62,17 @@ public class TripServiceTest {
         tripService.addTrip(createTripRequest);
         verify(tripRepository).insert(trip);
     }
+
+    @Test
+    void updateTrip() {;
+        tripService.updateTrip(updateTrip.get_id(), updateTripRequest);
+        Trip updatedTrip = Trip.builder()
+                ._id(id)
+                .userId(4)
+                .name("Mel T.")
+                .destination("Taipe")
+                .build();
+        verify(tripRepository).save(updatedTrip);
+    }
+
 }
