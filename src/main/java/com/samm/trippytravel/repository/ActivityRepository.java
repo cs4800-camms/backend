7a55f2dd08c8a5b9f38b078904d3a6d17d007158
stephaniedeleon <stephanie.deleon516@gmail.com>
2022-10-27 22:03:06 -0700

    implemented get activities with day_id endpoint

--- src/main/java/com/samm/trippytravel/repository/ActivityRepository.java
+++ src/main/java/com/samm/trippytravel/repository/ActivityRepository.java
@@ -2,8 +2,13 @@ package com.samm.trippytravel.repository;
 
 import com.samm.trippytravel.data.domain.Activity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ActivityRepository extends MongoRepository<Activity, Long> {
+    @Query(value="{ 'day_id' : ObjectId( ?0 )}")
+    List<Activity> getActivitiesByDayId(String dayId);
 }
--- src/main/java/com/samm/trippytravel/services/ActivityService.java
+++ src/main/java/com/samm/trippytravel/services/ActivityService.java
@@ -8,6 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ActivityService {
@@ -22,6 +26,10 @@ public class ActivityService {
                 .build()));
     }
 
+    public List<ActivityResponse> getActivitiesForDay(String dayIdNumber) {
+        return toListActivityResponse(activityRepository.getActivitiesByDayId(dayIdNumber));
+    }
+
     private ActivityResponse toActivityResponse(Activity activity) {
         return ActivityResponse.builder()
                 ._id(activity.get_id())
@@ -30,5 +38,11 @@ public class ActivityService {
                 .name(activity.getName())
                 .location(activity.getLocation())
                 .build();
+    }
+
+    private List<ActivityResponse> toListActivityResponse(List<Activity> activityList) {
+        return activityList.stream()
+                .map(this::toActivityResponse)
+                .collect(toList());
     }
 }
