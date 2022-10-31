b308ea648aff29fd5a20faabf613dcf9a6dc2ca9
stephaniedeleon <stephanie.deleon516@gmail.com>
2022-10-31 12:21:43 -0700

    implemented update user endpoint

--- src/main/java/com/samm/trippytravel/controllers/UserController.java
+++ src/main/java/com/samm/trippytravel/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.samm.trippytravel.controllers;
 
 import com.samm.trippytravel.data.requests.user.CreateUserRequest;
+import com.samm.trippytravel.data.requests.user.UpdateUserRequest;
 import com.samm.trippytravel.data.response.UserResponse;
 import com.samm.trippytravel.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,13 @@ public class UserController {
     public ResponseEntity<UserResponse> createUser(
             @RequestBody CreateUserRequest createUserRequest) {
         return new ResponseEntity<>(userService.addUser(createUserRequest), HttpStatus.OK);
+    }
+
+    @PostMapping("{userIdNumber}/update")
+    @ResponseBody
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable("userIdNumber") String userIdNumber,
+            @RequestBody UpdateUserRequest updateUserRequest) {
+        return new ResponseEntity<>(userService.updateUser(userIdNumber, updateUserRequest), HttpStatus.OK);
     }
 }
--- src/main/java/com/samm/trippytravel/services/UserService.java
+++ src/main/java/com/samm/trippytravel/services/UserService.java
@@ -2,6 +2,7 @@ package com.samm.trippytravel.services;
 
 import com.samm.trippytravel.data.domain.User;
 import com.samm.trippytravel.data.requests.user.CreateUserRequest;
+import com.samm.trippytravel.data.requests.user.UpdateUserRequest;
 import com.samm.trippytravel.data.response.UserResponse;
 import com.samm.trippytravel.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,16 @@ public class UserService {
                 .lastName(createUserRequest.getLastName())
                 .email(createUserRequest.getEmail())
                 .password(createUserRequest.getPassword())
+                .build()));
+    }
+
+    public UserResponse updateUser(String userIdNumber, UpdateUserRequest updateUserRequest) {
+        return toUserResponse(userRepository.save(User.builder()
+                ._id(userIdNumber)
+                .firstName(updateUserRequest.getFirstName())
+                .lastName(updateUserRequest.getLastName())
+                .email(updateUserRequest.getEmail())
+                .password(updateUserRequest.getPassword())
                 .build()));
     }
 
