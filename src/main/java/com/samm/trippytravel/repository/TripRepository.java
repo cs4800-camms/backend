072837c37870c5c13096edfa4671a4d6e472bbf8
stephaniedeleon <stephanie.deleon516@gmail.com>
2022-11-01 16:46:57 -0700

    implemented a get trip endpoint based on id

--- src/main/java/com/samm/trippytravel/repository/TripRepository.java
+++ src/main/java/com/samm/trippytravel/repository/TripRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TripRepository extends MongoRepository<Trip, Long> {
     @Query(value="{'_id' : ?0}", delete = true)
     Trip deleteById(String id);
+
+    @Query(value="{'_id' : ?0}")
+    Trip getTripById(String tripId);
 }
